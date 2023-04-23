827979bb2ad000c0e8767fe6f58c3b479419e4ff
Mitchell <myogen@Mitchells-MacBook-Pro.local>
2023-04-23 18:16:47 +0800

    Updates

---
+++ precache-manifest.77d584aa4a0215c8a7dba6bc9497398e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5a174b44bd73c97aad5",
+    "url": "/standchart-robot-vacuum/static/css/main.ca99bbb3.chunk.css"
+  },
+  {
+    "revision": "d5a174b44bd73c97aad5",
+    "url": "/standchart-robot-vacuum/static/js/main.d5a174b4.chunk.js"
+  },
+  {
+    "revision": "00c9e5765aa05acfddd8",
+    "url": "/standchart-robot-vacuum/static/js/1.00c9e576.chunk.js"
+  },
+  {
+    "revision": "9c7e8fc4dbaa45362371",
+    "url": "/standchart-robot-vacuum/static/js/runtime~main.9c7e8fc4.js"
+  },
+  {
+    "revision": "f88156d59a5eb0f2e590b1a3e4988688",
+    "url": "/standchart-robot-vacuum/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53f2ac1372fa4f090372c151f7c39788.js"
+  "/standchart-robot-vacuum/precache-manifest.77d584aa4a0215c8a7dba6bc9497398e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/standchart-robot-vacuum/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
