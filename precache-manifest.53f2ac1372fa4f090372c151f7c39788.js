da22f9e90f036a5ef0ac3dbeca839568f7a979d0
Mitchell Yogen <myogen@Mitchells-MacBook-Pro.local>
2023-04-23 17:52:50 +0800

    Updates

---
+++ precache-manifest.53f2ac1372fa4f090372c151f7c39788.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5a174b44bd73c97aad5",
+    "url": "/static/css/main.ca99bbb3.chunk.css"
+  },
+  {
+    "revision": "d5a174b44bd73c97aad5",
+    "url": "/static/js/main.d5a174b4.chunk.js"
+  },
+  {
+    "revision": "00c9e5765aa05acfddd8",
+    "url": "/static/js/1.00c9e576.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d27a49f66a2d4c54c1097d577f2a126d",
+    "url": "/index.html"
+  }
+];
